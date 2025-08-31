58665b549b0d699cd25f29135ef7cf80a5346f20
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2025-08-31 21:00:34 +0000

    Adjusted dj-database-url to aid Render deployment.

--- api_project/settings.py
+++ api_project/settings.py
@@ -102,11 +102,19 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-'''
 
 DATABASES = {
     "default": dj_database_url.config(
         default=config("DATABASE_URL")
+    )
+}
+'''
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
